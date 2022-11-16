014e1fe74a912c9524399e4204b610f8f9c083c9
AleksandrEgorov <ferociousrat@mail.ru>
2022-11-16 18:43:56 +0300

    dz asy 2

--- asyn.py
+++ asyn.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-d_base = 'postgresql+asyncpg://postgres:postgres@127.0.0.1:1111/star_wars'
+d_base = 'postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/star_wars'
 engine = create_async_engine(d_base)
 
 Base = declarative_base()
@@ -23,9 +23,9 @@ class StarWars(Base):
     films = Column(String)
     gender = Column(String)
     hair_color = Column(String)
-    height = Column(Integer)
+    height = Column(String)
     homeworld = Column(String)
-    mass = Column(Integer)
+    mass = Column(String)
     skin_color = Column(String)
     species = Column(String)
     starships = Column(String)
@@ -52,9 +52,29 @@ async def main():
     number = 1
     for item in range(0, get_count()):
         res = await get_stuff(number)
+
+        fil = ', '.join(res['films'])
+        spe = ', '.join(res['species'])
+        sta = ', '.join(res['starships'])
+        veh = ', '.join(res['vehicles'])
+
         number += 1
-        async with Session as session:
-            session.add()
+        async with Session() as session:
+            session.add(StarWars(
+                name=res['name'],
+                birth_year=res['birth_year'],
+                eye_color=res['eye_color'],
+                films=str(fil),
+                gender=res['gender'],
+                hair_color=res['hair_color'],
+                height=res['height'],
+                homeworld=res['homeworld'],
+                mass=res['mass'],
+                skin_color=res['birth_year'],
+                species=str(spe),
+                starships=str(sta),
+                vehicles=str(veh)
+            ))
             await session.commit()
 
 asyncio.run(main())
